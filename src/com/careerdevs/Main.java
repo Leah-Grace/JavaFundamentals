badfe1b449d86bcb03317cd5ee3846d662b21422
Leah_Grace <leahgdg@hotmail.com>
2022-01-03 11:51:45 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,12 @@
+package com.careerdevs;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from the main class!");
+    }
+
+
+
+}
---
+++ src/com/careerdevs/Test.java
@@ -0,0 +1,8 @@
+package com.careerdevs;
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello from the Test class!");
+    }
+}
